725b8691c4c838b6b5f38579494a4616fe2607a3
Sofya Kulachenko <kittsi2017@yandex.ru>
2025-03-30 20:09:40 +0300

    Initial commit.Created main.py

---
+++ data.py
@@ -0,0 +1,9 @@
+class LoginData:
+    user_name = Kitsi
+    email = "K123456@ya.ru"
+    password = "K123456"
+
+class ValidData:
+    user_name = 'Test test'
+    login = f"Test_test{random.randint(10, 999)}@yandex.ru"
+    password = f"{random.randint(100, 999)}{random.randint(100, 999)}"
